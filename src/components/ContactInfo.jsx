b52d8ce8d7c89133d41fb342ba3f4b4e62ccb496
Sarah <168814993+sarahwkh9@users.noreply.github.com>
2025-08-21 00:06:09 +0300

    Update ContactInfo.jsx

--- src/components/ContactInfo.jsx
+++ src/components/ContactInfo.jsx
@@ -4,7 +4,7 @@ export function ContactInfo() {
     return (
         <div className="d-flex flex-column align-items-center">
             <h2 className="fs-1 mb-3 text-uppercase fw-bold">Where To Find Us</h2>
-            <p className="mb-5">47 Baker Street, London LD1 0ND</p>
+            <p className="mb-5">University street, Jubieha Amman</p>
             <h3>Opening Hours</h3>
             <p className="m-0">Mon - Fri: 09:00 - 17:00</p>
             <p>Sat - Sun: 10:00 - 15:00</p>
